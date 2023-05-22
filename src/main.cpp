947e0d618d63cedb2b50f60e72bc18359fdb9da0
mdwalters <mail@mdwalters.ml>
2023-05-22 14:28:08 -0400

    chore: still has a compilation error

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <wiiuse/wpad.h>
 #include <grrlib.h>
 
-// static void *xfb = NULL;
-// static GXRModeObj *rmode = NULL;
-
-void init_video() {
-    VIDEO_Init();
-    WPAD_Init();
-    rmode = VIDEO_GetPreferredMode(NULL);
-    // xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
-    console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
-    VIDEO_Configure(rmode);
-    VIDEO_SetNextFramebuffer(xfb);
-    VIDEO_SetBlack(FALSE);
-    VIDEO_Flush();
-    VIDEO_WaitVSync();
-    if (rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
-    printf("\x1b[2;0H");
-}
-
 int main() {
-    init_video();
-
-    printf("osu!wii\n");
+    GRRLIB_Init();
+    WPAD_Init();
 
     while(1) {
         WPAD_ScanPads();
-        if (WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME) exit(0);
+        if (WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME) break;
+
+        // Our stuff here, I guess
+
+        GRRLIB_Render();
     }
+
+    GRRLIB_Exit();
+    exit(0);
 }
